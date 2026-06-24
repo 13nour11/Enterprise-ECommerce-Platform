fa2c01a66b9c9950f5a9f53ebc4b89a695b1851b
Nourhan Fahmy <110133351+13nour11@users.noreply.github.com>
2026-06-24 14:14:52 +0300

    Add files via upload
    Session 1
    Add config server
    Add Eureka Server
    Add Product Server
    Add Product Service

---
+++ product-service/src/main/java/com/example/product_service/models/Product.java
@@ -0,0 +1,10 @@
+package com.example.product_service.models;
+import java.math.BigDecimal;
+
+public record Product(
+        Long id,
+        String name,
+        String description,
+        BigDecimal price,
+        String category
+) {}
